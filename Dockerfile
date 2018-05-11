c6166c6b741a46c7b6da5b6b6d55b9c0c01cad05
flaviostutz <flaviostutz@gmail.com>
2018-05-11 08:40:15 -0300

    first mqtt bridge working

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:8.6.0
 
 ENV work_dir /usr/src/app/
+ENV KAFKA_ZOOKEEPER_CONNECT localhost:2181
 
 WORKDIR ${work_dir}
 
@@ -9,5 +10,6 @@ ADD src/package.json ${work_dir}
 RUN npm install
 
 ADD src/ ${work_dir}
+EXPOSE 1883 8883 80 443
 
 CMD ["npm", "start"]
--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log("TEST");
+console.log("Starting MQTT Mosca <-> Kafka server");
+
+var mosca = require('mosca');
+var ascoltatori = require('ascoltatori');
+
+var settings = {
+    type: 'kafka',
+    json: false,
+    kafka: require("kafka-node"),
+    connectionString: "${KAFKA_ZOOKEEPER_CONNECT}",
+    clientId: "ascoltatori",
+    groupId: "ascoltatori",
+    defaultEncoding: "utf8",
+    encodings: {
+        image: "buffer"
+    }
+};
+
+var settings = {
+    port: 1883,
+    backend: ascoltatori
+};
+
+var server = new mosca.Server(settings);
+
+server.on('clientConnected', function (client) {
+    console.log('client connected', client.id);
+});
+
+// fired when a message is received
+server.on('published', function (packet, client) {
+    console.log('Published', packet.payload);
+});
+
+server.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+    console.log('Mosca server is up and running');
+}
